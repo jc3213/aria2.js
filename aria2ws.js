1c307ee0183912e35283ac85582ca6335c0bb573
jc3213 <jc3213@users.noreply.github.com>
2024-11-02 22:21:18 +0800

    update 0.8.0

--- aria2ws.js
+++ aria2ws.js
@@ -10,27 +10,25 @@ class Aria2WebSocket {
     version = '0.8.0';
     timeout = 10000;
     connect () {
-        this.socket = new Promise((resolve, reject) => {
-            let ws = new WebSocket(this.jsonrpc);
-            ws.onopen = (event) => {
-                this.alive = true;
-                if (typeof this._onopen === 'function') { this._onopen(event); }
-                resolve(ws);
-            };
-            ws.onmessage = (event) => {
-                let response = JSON.parse(event.data);
-                if (!response.method) { ws.resolve(response); }
-                else if (typeof this._onmessage === 'function') { this._onmessage(response); }
-            };
-            ws.onclose = (event) => {
-                this.alive = false;
-                if (!event.wasClean) { setTimeout(() => this.connect(), this.timeout); }
-                if (typeof this._onclose === 'function') { this._onclose(event); }
-            };
-        });
+        this.socket = new WebSocket(this.jsonrpc);
+        this.socket.onopen = (event) => {
+            this.alive = true;
+            if (typeof this._onopen === 'function') { this._onopen(event); }
+            resolve(ws);
+        };
+        this.socket.onmessage = (event) => {
+            let response = JSON.parse(event.data);
+            if (!response.method) { this.socket.resolve(response); }
+            else if (typeof this._onmessage === 'function') { this._onmessage(response); }
+        };
+        this.socket.onclose = (event) => {
+            this.alive = false;
+            if (!event.wasClean) { setTimeout(() => this.connect(), this.timeout); }
+            if (typeof this._onclose === 'function') { this._onclose(event); }
+        };
     }
     disconnect () {
-        this.socket.then( (ws) => ws.close() );
+        this.socket?.close();
     }
     set onopen (callback) {
         this._onopen = typeof callback === 'function' ? callback : null;
@@ -51,10 +49,10 @@ class Aria2WebSocket {
         return typeof this._onclose === 'function' ? this._onclose : null;
     }
     send (...args) {
-        return this.socket.then((ws) => new Promise((resolve, reject) => {
-            ws.resolve = resolve;
-            ws.onerror = reject;
-            ws.send( JSON.stringify( args.map( ({ method, params = [] }) => ({id: '', jsonrpc: '2.0', method, params: [...this.params, ...params]}) ) ) );
-        }));
+        return new Promise((resolve, reject) => {
+            this.socket.resolve = resolve;
+            this.socket.onerror = reject;
+            this.socket.send( JSON.stringify( args.map( ({ method, params = [] }) => ({id: '', jsonrpc: '2.0', method, params: [...this.params, ...params]}) ) ) );
+        });
     }
 }
