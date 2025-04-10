37ca306b7d747ef7b3d9c36e943055890feb9525
jc3213 <jc3213@users.noreply.github.com>
2025-04-11 00:29:11 +0800

    Update aria2_0.8.0.js

--- archived/aria2_0.8.0.js
+++ archived/aria2_0.8.0.js
@@ -1,7 +1,7 @@
 class Aria2 {
     constructor (...args) {
         let path = args.join('#').match(/^(https?|wss?)(?:#|:\/\/)([^#]+)#?(.*)$/);
-        if (!path) { throw new Error('Malformed JSON-RPC entry: "' + args.join('", "') + '"'); }
+        if (!path) { throw new Error('Invalid JSON-RPC entry: "' + args.join('", "') + '"'); }
         this.scheme = path[1];
         this.url = path[2];
         this.secret = path[3];
@@ -11,7 +11,7 @@ class Aria2 {
     events = { onopen: null, onmessage: null, onclose: null };
     set scheme (scheme) {
         this.call = { 'http': this.post, 'https': this.post, 'ws': this.send, 'wss': this.send }[ scheme ];
-        if (!this.call) { throw new Error('Unsupported JSON-RPC scheme: "' + scheme + '"'); }
+        if (!this.call) { throw new Error('Invalid JSON-RPC scheme: "' + scheme + '" is not supported!'); }
         this.jsonrpc.scheme = scheme;
         this.jsonrpc.path = scheme + '://' + this.jsonrpc.url;
     }
