22581642038fecdf7f41cdef8af8dab8e89ebcc3
jc3213 <jc3213@users.noreply.github.com>
2025-09-03 15:37:53 +0800

    Update aria2c.cmd

--- cli/aria2c.cmd
+++ cli/aria2c.cmd
@@ -5,9 +5,9 @@ for /f "delims=" %%a in ('dir /s /b aria2c.exe') do (set aria2c=%%a)
 if not defined aria2c goto :eof
 set aria2c=!aria2c:%~dp0=!
 if not exist aria2c.session type nul > aria2c.session
-if [%1] equ [/s] goto :nowindow
-if [%1] equ [/r] goto :register
-if [%1] equ [/u] goto :Unregister
+if "%1" equ "/s" goto :nowindow
+if "%1" equ "/r" goto :register
+if "%1" equ "/u" goto :Unregister
 "%aria2c%" --conf=aria2c.conf
 :register
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG_SZ" /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"%aria2c%\",\"--conf=aria2c.conf\",\"%~dp0",\"\",0)(window.close)" /f
@@ -15,5 +15,5 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG
 mshta vbscript:CreateObject("Shell.Application").ShellExecute("%aria2c%","--conf=aria2c.conf","%~dp0","",0)(window.close)
 exit
 :unregister
-taskkill /im "aria2c.exe"
+taskkill /f /im "aria2c.exe" 2>nul
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /f
