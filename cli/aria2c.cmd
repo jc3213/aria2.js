2ff2be3e814f8c1873beb211ab7f934bfde6dbad
jc3213 <jc3213@users.noreply.github.com>
2025-09-10 10:44:43 +0800

    Update aria2c.cmd

--- cli/aria2c.cmd
+++ cli/aria2c.cmd
@@ -1,19 +1,16 @@
 @echo off
-setlocal enabledelayedexpansion
 pushd %~dp0
-for /f "delims=" %%a in ('dir /s /b /a-d aria2c.exe') do (set aria2c=%%a)
-if not defined aria2c goto :eof
-set aria2c=!aria2c:%~dp0=!
+if not exist bin\aria2c.exe goto :eof
 if not exist aria2c.session type nul > aria2c.session
 if "%1" equ "/s" goto :nowindow
 if "%1" equ "/r" goto :register
-if "%1" equ "/u" goto :Unregister
-"%aria2c%" --conf=aria2c.conf
+if "%1" equ "/u" goto :unregister
+bin\aria2c.exe --conf=aria2c.conf
 :register
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG_SZ" /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"%aria2c%\",\"--conf=aria2c.conf\",\"%~dp0",\"\",0)(window.close)" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG_SZ" /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"bin\\aria2c.exe\",\"--conf=aria2c.conf\",\"%~dp0",\"\",0)(window.close)" /f
 :nowindow
-mshta vbscript:CreateObject("Shell.Application").ShellExecute("%aria2c%","--conf=aria2c.conf","%~dp0","",0)(window.close)
-exit
+mshta vbscript:CreateObject("Shell.Application").ShellExecute("bin\\aria2c.exe","--conf=aria2c.conf","%~dp0","",0)(window.close)
+goto :eof
 :unregister
 taskkill /f /im "aria2c.exe" 2>nul
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /f
