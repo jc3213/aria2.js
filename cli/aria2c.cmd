d4fc1d8686f377ad9136098568cd9cdea7b63daa
jc3213 <jc3213@users.noreply.github.com>
2025-11-23 21:25:57 +0800

    Update aria2c.cmd

--- cli/aria2c.cmd
+++ cli/aria2c.cmd
@@ -2,15 +2,15 @@
 pushd %~dp0
 if not exist aria2c.exe goto :eof
 if not exist aria2c.session type nul > aria2c.session
-if "%1" equ "/s" goto :nowindow
-if "%1" equ "/r" goto :register
-if "%1" equ "/u" goto :unregister
+if "%1" equ "/s" goto :silent
+if "%1" equ "/r" goto :install
+if "%1" equ "/u" goto :uninstall
 aria2c.exe --conf=aria2c.conf
-:register
+:install
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG_SZ" /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"aria2c.exe\",\"--conf=aria2c.conf\",\"%~dp0",\"\",0)(window.close)" /f
-:nowindow
+:silent
 mshta vbscript:CreateObject("Shell.Application").ShellExecute("aria2c.exe","--conf=aria2c.conf","%~dp0","",0)(window.close)
 goto :eof
-:unregister
+:uninstall
 taskkill /f /im "aria2c.exe" 2>nul
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /f
