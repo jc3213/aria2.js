600585c0918203461b92df280756aace45e95d6c
jc3213 <jc3213@users.noreply.github.com>
2026-07-16 08:45:25 +0800

    Update worker.js

--- sharedworker/worker.js
+++ sharedworker/worker.js
@@ -2,6 +2,7 @@ const aria2 = (() => {
     let pending = {};
     let hash = Date.now().toString(36) + '-' + Math.random().toString(36).substring(2);
     let index = 0;
+
     let retries = 10;
     let timeout = 10;
     let events = {};
